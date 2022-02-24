7ab92da642534468024a7da76e0f39252c95b497
co-bby <nkumsagoefredrick@gmail.com>
2022-02-24 10:05:18 -0500

    fixed a database connection error

--- DB/connect.js
+++ DB/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose")
 
-const connectionString =
-  "mongodb+srv://Cobby:nkum32eee@cluster0.9t7ho.mongodb.net/Task-Manager?retryWrites=true&w=majority"
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to the DB"))
-  .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require("./DB/connect")
-
 const express = require("express")
 const app = express()
 const tasks = require("./routes/tasks")
+const connectDB = require("./db/connect")
+require("dotenv").config()
 //middleware
 app.use("/api/v1/tasks", tasks)
 app.use(express.static("./public"))
@@ -14,4 +14,15 @@ app.get("/hello", (req, res) => {
 })
 
 const port = 3000
-app.listen(port, console.log(`server is listening on port ${port}`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
