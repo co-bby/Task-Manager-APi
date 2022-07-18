be6435f863730a4b82714c2c3816442322f9d406
co-bby <nkumsagoefredrick@gmail.com>
2022-07-17 23:20:00 -0400

    added delete task controller

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('./public'));
 
 // routes
 app.get('/hello', (req, res) => {
-  res.send('hello world!');
+  res.send('hello world');
 });
 
 const port = process.env.PORT || 5000;
@@ -18,9 +18,7 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, () => console.log(`listening to port ${port}`));
   } catch (error) {
     console.log(error);
   }
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -21,16 +21,29 @@ const getTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
-    res.status(200).json({ task });
+
     if (!task) {
-      return res.status(404).json({ msg: 'No task with id: ${taskID}' });
+      return res.status(500).json({ msg: `No task with id: ${taskID}` });
     }
-  } catch (err) {}
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
+
 const updateTask = (req, res) => {
   res.send('update task');
 };
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(500).json({ msg: `No task found with id ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
 };
 module.exports = { getAllTasks, createTask, deleteTask, updateTask, getTask };
