1c684aee3a74e8b109412ebdae600fa0c858beeb
co-bby <nkumsagoefredrick@gmail.com>
2022-07-22 00:01:59 -0400

    added error handler

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 const notfound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
 //middleware
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 
 app.use(express.static('./public'));
 app.use(notfound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
---
+++ middleware/error-handler.js
@@ -0,0 +1,4 @@
+const errorHandlerMiddleware = (err, req, res, next) => {
+  return res.status(500).json({ msg: err.message });
+};
+module.exports = errorHandlerMiddleware;
